779fe62f82f5b56141a24f3486e394bda8277cde
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-07-14 15:34:52 -0500

    all api requests done need to add to database now

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -6,6 +6,20 @@ const router = express.Router();
 
 //CREATE - POST
 router.post('/', (req, res) => {
+  const newCar = req.body;
+  db("cars")
+  .insert(newCar, "id")
+  .then(ids => {
+    db("cars")
+    .where({ id: ids[0] })
+    .first()
+    .then( car => {
+      res.status(200).json({ data: car })
+    })
+    .catch(error => {
+      res.status(500).json({ message: "there was an error making the car" })
+    })
+  })
  
 });
 
@@ -17,20 +31,57 @@ router.get('/', (req, res) => {
     res.status(200).json(cars);
   })
   .catch(error => {
-    res.status(500).json({ message: 'THere was an error getting the cars', error: error.message })
+    res.status(500).json({ message: 'There was an error getting the cars', error: error.message })
   });
 });
 
-router.get('/:id', (req, res) => {});
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  db.select("*")
+  .from("cars")
+  .where({ id })
+  .first()
+  .then(car => 
+    res.status(200).json({ data: car }))
+  .catch(error => {
+    res.status(500).json({ message: "could not the car with that id" })
+  })
+});
 
 
 //STRETCH
 //UPDATE - PUT
 
-router.put('/', (req, res) => {});
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  db("cars")
+  .where({ id })
+  .update(changes)
+  .then( count => {
+    count > 0 ? res.status(200).json({ data: count }) :
+    res.status(404).json({ message: "There was no car to update" })
+  })
+  .catch(error => {
+    res.status(500).json({ message: "Could not update the car", error: error.message })
+  })
+});
 
 //DELETE - DELETE
-router.delete('/', (req, res) => {});
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  db("cars")
+  .where({ id })
+  .del()
+  .then(count => {
+    count > 0 ? res.status(204).json({ data: count }) :
+    res.status(404).json({ message:"Car could not be deleted" })
+  })
+  .catch(error => {
+    res.status(500).json({ message: "There was an error deleting the car", error: error.message })
+  })
+});
 
 
 module.exports = router;
