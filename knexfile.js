80f002b0c5914e9ffc07f02ecb8e698150306fce
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-07-14 13:54:26 -0500

    added and configured knex file

--- knexfile.js
+++ knexfile.js
@@ -5,40 +5,16 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      filename: './data/car-dealer.db3' //configure knex to connect to the database
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    //need for Sqlite
+    useNullAsDefault: true,
     migrations: {
-      tableName: 'knex_migrations'
+        directory: './data/migrations'
     }
-  },
 
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
   }
+
+ 
 
 };
